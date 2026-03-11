7e3f678f3904aed01c4a5dfc9999dbfc09e338be
dalianyizhang <1879561193@qq.com>
2026-03-12 01:37:28 +0800

    修改下载和打包结构，使zip解压后直接包含各项文件

--- pss-01-pss/src/main/java/crawl/Main.java
+++ pss-01-pss/src/main/java/crawl/Main.java
@@ -13,7 +13,7 @@ import java.time.format.DateTimeFormatter;
 public class Main {
     public static void main(String[] args) {
         String date = LocalDate.now(ZoneOffset.UTC).format(DateTimeFormatter.BASIC_ISO_DATE);
-        String downloadPath = "downloads/" + date;
+        String downloadPath = "downloads";
 
         // 下载并解析“船员信息+合成信息”文件到本地
         PSSCrawlFileAPI.crawlCrewAndPrestige(downloadPath);
